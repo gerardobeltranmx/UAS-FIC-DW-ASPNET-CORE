2028b22cb78b3f831d83428d31564f369c489ce7
gerardobeltran <contacto@gerardobeltran.mx>
2021-07-03 17:39:19 -0600

    -

--- APIWeb/Controllers/CategoriasController.cs
+++ APIWeb/Controllers/CategoriasController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using APIWeb.Helpers;
 using APIWeb.Models;
 using APIWeb.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace APIWeb.Controllers
 {
