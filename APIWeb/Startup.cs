207932a2fca9c0892d0b8ceb4d2b6c05779b12f4
gerardobeltran <contacto@gerardobeltran.mx>
2021-07-04 02:05:26 -0600

    -

--- APIWeb/Startup.cs
+++ APIWeb/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-// pa ver
+// pa ver fdsfsd
 namespace APIWeb
 {
     public class Startup
