c0a12dd0f2afa4bc75370ae1c4d135f375c4569e
gerardobeltran <contacto@gerardobeltran.mx>
2021-07-04 01:41:27 -0600

    Inserción inicial en el repositorio del módulo Surtidor

---
+++ APIWeb/Helpers/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using APIWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIWeb.Helpers
+{// comentariodsfsdfsd
+    public class Datos : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; } // tabla categoria la base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string Conexion = @"Server = surtidor.database.windows.net;
+                                Database = surtidordb;
+                                User = admindb;
+                                Password = 12AB34cd*;";
+            optionsBuilder.UseSqlServer(Conexion);
+        }
+    }
+}
