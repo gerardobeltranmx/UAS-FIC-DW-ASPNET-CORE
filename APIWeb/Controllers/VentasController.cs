8e3e0b4eff2bf9dda2cc6feb2fca10b0a4eb309e
gerardobeltran <contacto@gerardobeltran.mx>
2021-07-24 12:37:13 -0600

    --

--- APIWeb/Controllers/VentasController.cs
+++ APIWeb/Controllers/VentasController.cs
@@ -19,9 +19,7 @@ namespace APIWeb.Controllers
         {
             Respuesta Resultado = new Respuesta();
             Datos db = new Datos();
-            var ventas = from v in db.Ventas
-                         join dv in db.Ventas_Detalles
-                           on v.id equals dv.idventa
+            var ListaVentas = from v in db.Ventas
                          select new
                          {
                              v.id,
@@ -33,11 +31,40 @@ namespace APIWeb.Controllers
                          };
 
 
-            Resultado.Info = ventas;
+            Resultado.Info = ListaVentas;
 
 
             return Ok(Resultado);
         }
+
+        [HttpGet("[action]/{idVenta}")]
+        public ActionResult Detalles(int idVenta)
+        {
+            Respuesta Resultado = new Respuesta();
+            Datos db = new Datos();
+            var ListaDetalles = from d in db.Ventas_Detalles
+                              join p in db.Productos
+                                  on d.idproducto equals p.id
+                              where (d.idventa==idVenta)
+                              select new
+                              {
+                                  d.id,
+                                  d.idventa,
+                                  d.idproducto,
+                                  p.nombre,
+                                  d.cantidad,
+                                  d.precio,
+                                  d.descuento
+                              };
+
+
+            Resultado.Info = ListaDetalles;
+
+
+            return Ok(Resultado);
+        }
+
+
 
         // GET api/values/5
         [HttpGet("{id}")]
@@ -50,38 +77,48 @@ namespace APIWeb.Controllers
             [HttpPost("[action]")]
             public ActionResult Nueva([FromBody] VentaNuevaViewModel v)
         {
-            Datos db = new Datos();
-            var venta = new Venta()
+            Respuesta Resultado = new Respuesta();
+            try
             {
-                idcliente = v.idcliente,
-                idusuario = v.idusuario,
-                num_factura = v.num_factura,
-                fecha_hora = DateTime.Now,
-                impuesto = v.impuesto,
-                total = v.total
-            };
-            // Agrega y Guarda al venta
-            db.Ventas.Add(venta);
-            db.SaveChanges();
-
-            // Guardar los detalles de la venta
-            
-            foreach (var d in v.detalles)
-            {
-                var detalle = new Venta_Detalle()
+                Datos db = new Datos();
+                var venta = new Venta()
                 {
-                    idproducto = d.idproducto,
-                    idventa = venta.id,
-                    cantidad = d.cantidad,
-                    precio = d.precio,
-                    descuento = d.descuento
+                    idcliente = v.idcliente,
+                    idusuario = v.idusuario,
+                    num_factura = v.num_factura,
+                    fecha_hora = DateTime.Now,
+                    impuesto = v.impuesto,
+                    total = v.total
                 };
+                // Agrega y Guarda al venta
+                db.Ventas.Add(venta);
+                db.SaveChanges();
 
-                db.Ventas_Detalles.Add(detalle);
+                // Guardar los detalles de la venta
+
+                foreach (var d in v.detalles)
+                {
+                    var detalle = new Venta_Detalle()
+                    {
+                        idproducto = d.idproducto,
+                        idventa = venta.id,
+                        cantidad = d.cantidad,
+                        precio = d.precio,
+                        descuento = d.descuento
+                    };
+
+                    db.Ventas_Detalles.Add(detalle);
+                }
+                db.SaveChanges();
+
+                Resultado.Info = v;
             }
-            db.SaveChanges();
+            catch (Exception ex) {
+                Resultado.Estado = false;
+                Resultado.Mensaje = ex.Message;
+            }
 
-            return Ok(venta);
+            return Ok(Resultado);
         }
 
         // PUT api/values/5
--- APIWeb/Models/Venta_Detalle.cs
+++ APIWeb/Models/Venta_Detalle.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace APIWeb.Models
 {
+    [Table("ventasdetalles")]
     public class Venta_Detalle
     {
         public int id { get; set; }
-        [ForeignKey("venta")]
+        //[ForeignKey("venta")]
 
         public int idventa { get; set; }
         public int idproducto { get; set; }
